14c22aff894d98259f9cbad761704f4140f77e1d
oleksandr <oleksandr.mateik@rescrypto.pro>
2019-08-22 10:27:24 +0300

    chat

--- chatdemo/static/chatdemo/loadhistory.js
+++ chatdemo/static/chatdemo/loadhistory.js
@@ -1,46 +1,45 @@
-$(function() {
+$(function () {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
 
     var loadhistorysock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/loadhistory/");
 
-    loadhistorysock.onmessage = function(message) {
+    loadhistorysock.onmessage = function (message) {
 
         var data = JSON.parse(message.data);
 
         new_messages = data.messages
 
         last_id = data.previous_id
-        
-        if(last_id == -1){
+
+        if (last_id == -1) {
             $("#load_old_messages").remove();
             $("#last_message_id").text(last_id)
-            if(new_messages.length == 0){
+            if (new_messages.length == 0) {
                 return;
             }
-        }
-        else{
+        } else {
             $("#last_message_id").text(last_id)
         }
 
         var chat = $("#chat")
 
-        for(var i=new_messages.length - 1; i>=0; i--){
+        for (var i = new_messages.length - 1; i >= 0; i--) {
             var ele = $('<li class="list-group-item"></li>')
-            
+
             ele.append(
-                '<strong>'+new_messages[i]['user']+'</strong> : '
-                )
-            
+                '<strong>' + new_messages[i]['user'] + '</strong> : '
+            )
+
             ele.append(
                 new_messages[i]['message'])
-            
+
             chat.prepend(ele)
         }
 
     };
 
-    $("#load_old_messages").on("click", function(event) {
+    $("#load_old_messages").on("click", function (event) {
         var message = {
             last_message_id: $('#last_message_id').text()
         }
